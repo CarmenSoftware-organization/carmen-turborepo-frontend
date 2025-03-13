2fc536a10d91ae1cffcd8bda4f309a25e7c6dff2
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2025-03-13 13:38:32 +0700

    fix

--- apps/platform-frontend/app/(root)/layout.tsx
+++ apps/platform-frontend/app/(root)/layout.tsx
@@ -9,14 +9,12 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             <SidebarComponent />
             <div className="flex-1 flex flex-col overflow-auto">
                 <NavbarComponent />
-                <main className="p-8">
+                <main className="p-4">
                     <Suspense fallback={<p>Suspense Loading...</p>}>
                         {children}
                     </Suspense>
                 </main>
             </div>
-
-
         </SidebarProvider>
     );
 } 
