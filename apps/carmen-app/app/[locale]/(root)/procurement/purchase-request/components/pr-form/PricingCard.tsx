411892d8ca204435aa479d65c3edd0553c7d5d52
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2025-07-21 10:17:04 +0700

    Refactor PricingCard component: Simplify JSX structure by removing unnecessary elements and integrating PricingField and PricingCardSummary components for improved readability and maintainability.

--- apps/carmen-app/app/[locale]/(root)/procurement/purchase-request/components/pr-form/PricingCard.tsx
+++ apps/carmen-app/app/[locale]/(root)/procurement/purchase-request/components/pr-form/PricingCard.tsx
@@ -1,13 +1,11 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import { Card, CardContent, CardTitle, CardHeader } from "@/components/ui/card";
-import { Label } from "@/components/ui/label";
-import { Checkbox } from "@/components/ui/checkbox";
-import { DollarSign, TrendingUp } from "lucide-react";
+import { DollarSign } from "lucide-react";
 import { PurchaseRequestDetailItem } from "@/dtos/pr.dto";
 import { useCurrency } from "@/hooks/useCurrency";
 import { useVendor } from "@/hooks/useVendor";
 import { formType } from "@/dtos/form.dto";
+import PricingField from "./PricingField";
+import PricingCardSummary from "./PricingCardSummary";
 
 interface PricingCardProps {
     readonly item: PurchaseRequestDetailItem;
@@ -22,14 +20,9 @@ export default function PricingCard({ item, onItemUpdate, mode }: PricingCardPro
     return (
         <Card>
             <CardHeader>
-                <CardTitle className="flex items-center justify-between">
-                    <div className="flex items-center gap-2">
-                        <DollarSign className="text-green-500" />
-                        Pricing
-                    </div>
-                    <Button variant="outline" size="sm" className="w-fit h-7">
-                        <TrendingUp /> Vendor Comparison
-                    </Button>
+                <CardTitle >
+                    <DollarSign className="text-green-500" />
+                    Pricing
                 </CardTitle>
             </CardHeader>
             <CardContent className="space-y-4">
@@ -77,88 +70,27 @@ export default function PricingCard({ item, onItemUpdate, mode }: PricingCardPro
                         isAmount
                         mode={mode}
                     />
-                    <div className="space-y-1">
-                        <Label>Net Total</Label>
-                        <div className="bg-blue-50 p-2 rounded-md h-20 flex items-center justify-center">
-                            <p className="text-blue-700 font-bold text-xl">(mock) USD 100.00</p>
-                        </div>
-                    </div>
-
-                    <div className="space-y-1">
-                        <Label>Total Amount</Label>
-                        <div className="bg-green-50 p-2 rounded-md h-20 flex items-center justify-center">
-                            <p className="text-green-700 font-bold text-xl">(mock) USD 100.00</p>
-                        </div>
-                    </div>
+                    <PricingCardSummary
+                        label="Net Total"
+                        currencyCode={getCurrencyCode(item.currency_id)}
+                        value="(mock) USD 100.00"
+                        placeholder="Net Total"
+                        color="blue"
+                        isAmount
+                        mode={mode}
+                    />
+                    <PricingCardSummary
+                        label="Total Amount"
+                        currencyCode={getCurrencyCode(item.currency_id)}
+                        value="(mock) USD 100.00"
+                        placeholder="Total Amount"
+                        color="green"
+                        isAmount
+                        mode={mode}
+                    />
                 </div>
             </CardContent>
         </Card>
     );
 };
 
-interface PricingProps {
-    label: string;
-    value: string;
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    placeholder: string;
-}
-
-const PricingField = ({ label, value, onChange, placeholder }: PricingProps) => {
-    return (
-        <div className="space-y-1">
-            <Label>{label}</Label>
-            <Input
-                value={value}
-                onChange={onChange}
-                placeholder={placeholder}
-                className="text-sm h-8 border-blue-300 focus:border-blue-500"
-            />
-        </div>
-    );
-};
-
-interface PricingCardSummaryProps {
-    label: string;
-    currencyCode: string;
-    value: string;
-    placeholder: string;
-    color: string;
-    isAmount?: boolean;
-    mode: formType;
-}
-
-const PricingCardSummary = ({ label, currencyCode, value, placeholder, color, isAmount = false, mode }: PricingCardSummaryProps) => {
-
-    return (
-        <div className="space-y-1">
-            {mode !== formType.VIEW ? (
-                <>
-                    <Label>{label}</Label>
-                    <div className={`bg-${color}-50 p-2 rounded-md h-20 grid grid-cols-2 gap-2`}>
-                        <div className="space-y-1">
-                            <Label className={`text-${color}-700 font-semibold`}>{currencyCode}</Label>
-                            <Input value={value} placeholder={placeholder} />
-                        </div>
-                        {isAmount && (
-                            <div className="space-y-1">
-                                <Label className={`flex items-center gap-2 my-1 text-${color}-700 font-semibold`}>
-                                    <Checkbox />
-                                    Amount
-                                </Label>
-                                <Input />
-                            </div>
-                        )}
-                    </div>
-                </>
-            ) : (
-                <>
-                    <Label>{label}</Label>
-                    <div className={`bg-${color}-50 rounded-md h-20 flex items-center justify-center gap-2`}>
-                        <p className={`text-${color}-700 font-semibold`}>{currencyCode}</p>
-                        <p className={`text-${color}-700 font-semibold`}>{value}</p>
-                    </div>
-                </>
-            )}
-        </div>
-    )
-}
---
+++ apps/carmen-app/app/[locale]/(root)/procurement/purchase-request/components/pr-form/PricingCardSummary.tsx
@@ -0,0 +1,49 @@
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { formType } from "@/dtos/form.dto";
+
+interface PricingCardSummaryProps {
+    label: string;
+    currencyCode?: string;
+    value: string;
+    placeholder: string;
+    color: string;
+    isAmount?: boolean;
+    mode: formType;
+}
+
+export default function PricingCardSummary({ label, currencyCode, value, placeholder, color, isAmount, mode }: PricingCardSummaryProps) {
+    return (
+        <div className="space-y-1">
+            {mode !== formType.VIEW ? (
+                <>
+                    <Label>{label}</Label>
+                    <div className={`bg-${color}-50 p-2 rounded-md h-20 grid grid-cols-2 gap-2`}>
+                        <div className="space-y-1">
+                            <Label className={`text-${color}-700 font-semibold`}>{currencyCode}</Label>
+                            <Input value={value} placeholder={placeholder} />
+                        </div>
+                        {isAmount && (
+                            <div className="space-y-1">
+                                <Label className={`flex items-center gap-2 my-1 text-${color}-700 font-semibold`}>
+                                    <Checkbox />
+                                    Amount
+                                </Label>
+                                <Input />
+                            </div>
+                        )}
+                    </div>
+                </>
+            ) : (
+                <>
+                    <Label>{label}</Label>
+                    <div className={`bg-${color}-50 rounded-md h-20 flex items-center justify-center gap-2`}>
+                        <p className={`text-${color}-700 font-semibold`}>{currencyCode}</p>
+                        <p className={`text-${color}-700 font-semibold`}>{value}</p>
+                    </div>
+                </>
+            )}
+        </div>
+    )
+}
