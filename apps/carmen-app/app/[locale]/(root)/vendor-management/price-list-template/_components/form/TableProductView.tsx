0d196bf16c52c99d07bb589c7d01131f1b34ebec
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2025-11-27 11:23:13 +0700

    ui table product

--- apps/carmen-app/app/[locale]/(root)/vendor-management/price-list-template/_components/form/TableProductView.tsx
+++ apps/carmen-app/app/[locale]/(root)/vendor-management/price-list-template/_components/form/TableProductView.tsx
@@ -1,9 +1,51 @@
 import { InitialProduct } from "@/components/lookup/product-tree-moq/types";
+import { Badge } from "@/components/ui/badge";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { useTranslations } from "next-intl";
 
 interface Props {
   initialProducts?: InitialProduct[];
 }
 
 export default function TableProductView({ initialProducts = [] }: Props) {
-  return <pre>{JSON.stringify(initialProducts, null, 2)}</pre>;
+  const t = useTranslations("PriceListTemplate");
+
+  return (
+    <div className="rounded-md border">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>{t("product_name")}</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {initialProducts.length > 0 ? (
+            initialProducts.map((product) => (
+              <TableRow key={product.id}>
+                <TableCell>
+                  <div className="flex items-center gap-2">
+                    {product.name}
+                    <Badge variant="product_badge">{product.code || "-"}</Badge>
+                  </div>
+                </TableCell>
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={2} className="h-24 text-center">
+                {t("no_products")}
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
