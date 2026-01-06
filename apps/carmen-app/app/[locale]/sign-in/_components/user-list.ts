76ef850d8693422ac2a20695d6edd2536d2f14eb
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2026-01-06 09:37:37 +0700

    del data

--- apps/carmen-app/app/[locale]/sign-in/_components/user-list.ts
+++ apps/carmen-app/app/[locale]/sign-in/_components/user-list.ts
@@ -1,64 +1,29 @@
-import { PLATFORM_ROLE } from "@/constants/enum";
-
 export const userList = [
-    {
-        email: "newuser2@example.com",
-    },
-    {
-        email: "system-admin@blueledgers.com",
-        firstname: "system-admin",
-        lastname: "system-admin",
-        platform_role: PLATFORM_ROLE.PLATFORM_ADMIN,
-        created_by_id: null,
-    },
-    {
-        email: "test@test.com",
-        firstname: "test",
-        lastname: "test",
-        platform_role: PLATFORM_ROLE.USER,
-        created_by_id: "system_admin"
-    },
-    {
-        email: "admin@blueledgers.com",
-        firstname: "admin",
-        lastname: "admin",
-        platform_role: PLATFORM_ROLE.USER,
-        created_by_id: "system_admin"
-    },
-    {
-        email: "user1@blueledgers.com",
-        firstname: "user1",
-        lastname: "staff",
-        platform_role: PLATFORM_ROLE.USER,
-        created_by_id: "system_admin"
-    },
-    {
-        email: "user2@blueledgers.com",
-        firstname: "user2",
-        lastname: "department-manager",
-        platform_role: PLATFORM_ROLE.USER,
-        created_by_id: "system_admin"
-    },
-    {
-        email: "user3@blueledgers.com",
-        firstname: "user3",
-        lastname: "purchasing-staff",
-        platform_role: PLATFORM_ROLE.USER,
-        created_by_id: "system_admin"
-    },
-    {
-        email: "user4@blueledgers.com",
-        firstname: "user4",
-        lastname: "finance-manager",
-        platform_role: PLATFORM_ROLE.USER,
-        created_by_id: "system_admin"
-    },
-    {
-        email: "user5@blueledgers.com",
-        firstname: "user5",
-        lastname: "general-manager",
-        platform_role: PLATFORM_ROLE.USER,
-        created_by_id: "system_admin"
-    },
-
-]
+  {
+    email: "newuser2@example.com",
+  },
+  {
+    email: "system-admin@blueledgers.com",
+  },
+  {
+    email: "test@test.com",
+  },
+  {
+    email: "admin@blueledgers.com",
+  },
+  {
+    email: "user1@blueledgers.com",
+  },
+  {
+    email: "user2@blueledgers.com",
+  },
+  {
+    email: "user3@blueledgers.com",
+  },
+  {
+    email: "user4@blueledgers.com",
+  },
+  {
+    email: "user5@blueledgers.com",
+  },
+];
