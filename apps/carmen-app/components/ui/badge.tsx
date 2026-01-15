da62e2f6de6b062389e9d59930935476849b38e0
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2026-01-15 19:29:43 +0700

    responsive component

--- apps/carmen-app/components/ui/badge.tsx
+++ apps/carmen-app/components/ui/badge.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const badgeVariants = cva(
-  "text-[11px] inline-flex items-center rounded-full px-1.5 md:px-2.5 py-0 md:py-0.5 font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+  "text-[10px] xl:text-[11px] inline-flex items-center rounded-full px-1.5 xl:px-2.5 py-0 xl:py-0.5 font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
   {
     variants: {
       variant: {
--- apps/carmen-app/components/ui/data-grid-table.tsx
+++ apps/carmen-app/components/ui/data-grid-table.tsx
@@ -50,7 +50,7 @@ function DataGridTableBase({ children }: { children: ReactNode }) {
     <table
       data-slot="data-grid-table"
       className={cn(
-        "w-full align-middle caption-bottom text-left rtl:text-right text-foreground font-normal text-xs md:text-sm",
+        "w-full align-middle caption-bottom text-left rtl:text-right text-foreground font-normal text-xs xl:text-sm",
         !props.tableLayout?.columnsDraggable && "border-separate border-spacing-0",
         props.tableLayout?.width === "fixed" ? "table-fixed" : "table-auto",
         props.tableClassNames?.base
@@ -380,7 +380,7 @@ function DataGridTableLoader() {
 
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-      <div className="text-muted-foreground bg-card  flex items-center gap-2 px-4 py-2 font-medium leading-none text-xs md:text-sm border border-border shadow-xs rounded-md">
+      <div className="text-muted-foreground bg-card  flex items-center gap-2 px-4 py-2 font-medium leading-none text-xs xl:text-sm border border-border shadow-xs rounded-md">
         <svg
           className="animate-spin -ml-1 h-5 w-5 text-muted-foreground"
           xmlns="http://www.w3.org/2000/svg"
